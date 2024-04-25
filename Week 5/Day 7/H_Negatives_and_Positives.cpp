866d8f20f16a34d4dab05c99b1c7f91a591727aa
Hasib-Nabid <nabid4424@gmail.com>
2024-04-25 22:55:28 +0600

    .

---
+++ Week 5/Day 7/H_Negatives_and_Positives.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define float double
+#define endl '\n'
+#define pb push_back
+#define po pop_back
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define shesh cout << endl
+#define loop(i,n) for (int i =0; i < (n); ++i)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+typedef vector<int> vi;
+int gcd ( int a, int b ) { return __gcd ( a, b ); }
+int lcm ( int a, int b ) { return a * ( b / gcd ( a, b ) ); }
+const double PI = acos(-1);
+#define nabidtheboss() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) 
+
+
+int32_t main () 
+{
+
+    nabidtheboss();
+    w(t){
+        int n,cnt=0,sum=0;
+        cin>>n;
+        vi v(n);
+        loop(i,n){
+            cin>>v[i];
+            if(v[i]<0){
+                cnt++;
+            v[i]=abs(v[i]);
+        }
+        sum+=v[i];
+        }
+
+        sort(all(v));
+    
+        if(cnt%2==1) {
+            sum-=2*v[0];
+        }
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
